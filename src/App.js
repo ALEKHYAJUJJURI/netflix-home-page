2a17b073f2bcec04e3119a55d02fae1dbbfcc905
Alekhya <jujjurialekhya123@gmail.com>
2024-10-21 00:08:49 +0530

    commit-4

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import bootstrap from 'bootstrap';
+
 import { Middle } from './middle';
 
 export default function App(){
